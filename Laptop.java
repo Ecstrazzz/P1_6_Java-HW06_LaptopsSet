32a3709ae55c29378054245d9bc949341be4e700
Ecs <ecstrazzz@ya.ru>
2024-07-21 21:37:34 +0500

    Extended the notebook class, redefined the method

--- Laptop.java
+++ Laptop.java
@@ -24,40 +24,128 @@
  * 
  */
 
-public class Laptop {
-
-    private String brand;
+ public class Laptop {
+    private String brand; // Бренд
+    private String model; // Модель
+    private String os; // Операционная система
     private int ram; // ОЗУ в ГБ
     private int storage; // Объем ЖД в ГБ
-    private String os; // Операционная система
     private String color; // Цвет
+    private double screenSize; // Размер экрана в дюймах
+    private String resolution; // Разрешение экрана
+    private String cpuType; // Тип процессора
+    private double price; // Цена
 
-    public Laptop(String brand, int ram, int storage, String os, String color) {
+    // Конструктор
+    public Laptop(String brand, String model, String os, int ram, int storage, 
+                  String color, double screenSize, String resolution, 
+                  String cpuType, double price) {
         this.brand = brand;
+        this.model = model;
+        this.os = os;
         this.ram = ram;
         this.storage = storage;
-        this.os = os;
         this.color = color;
+        this.screenSize = screenSize;
+        this.resolution = resolution;
+        this.cpuType = cpuType;
+        this.price = price;
     }
 
+    // Методы-геттеры и сеттеры
     public String getBrand() {
         return brand;
     }
 
-    public int getRam() {
-        return ram;
+    public void setBrand(String brand) {
+        this.brand = brand;
     }
 
-    public int getStorage() {
-        return storage;
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
     }
 
     public String getOs() {
         return os;
     }
 
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getStorage() {
+        return storage;
+    }
+
+    public void setStorage(int storage) {
+        this.storage = storage;
+    }
+
     public String getColor() {
         return color;
     }
 
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getScreenSize() {
+        return screenSize;
+    }
+
+    public void setScreenSize(double screenSize) {
+        this.screenSize = screenSize;
+    }
+
+    public String getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution;
+     }
+     
+     public String getCpuType() {
+         return cpuType;
+     }
+     
+     public void setCpuType(String cpuType) {
+         this.cpuType = cpuType;
+     }
+     
+     public double getPrice() {
+         return price;
+     }
+     
+     public void setPrice(double price) {
+         this.price = price;
+     }
+     
+     @Override
+     public String toString() {
+         return "Laptop{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", os='" + os + '\'' +
+                ", ram=" + ram +
+                ", storage=" + storage +
+                ", color='" + color + '\'' +
+                ", screenSize=" + screenSize +
+                ", resolution='" + resolution + '\'' +
+                ", cpuType='" + cpuType + '\'' +
+                ", price=" + price +
+                '}';
+     }
 }
