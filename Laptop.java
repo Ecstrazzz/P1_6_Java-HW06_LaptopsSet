1853e3b1cc32760480a3927055606fe232fda819
Ecs <ecstrazzz@ya.ru>
2024-07-21 21:04:41 +0500

    Added an entry-level class of laptopsp

--- Laptop.java
+++ Laptop.java
@@ -26,5 +26,38 @@
 
 public class Laptop {
 
-    
+    private String brand;
+    private int ram; // ОЗУ в ГБ
+    private int storage; // Объем ЖД в ГБ
+    private String os; // Операционная система
+    private String color; // Цвет
+
+    public Laptop(String brand, int ram, int storage, String os, String color) {
+        this.brand = brand;
+        this.ram = ram;
+        this.storage = storage;
+        this.os = os;
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public int getStorage() {
+        return storage;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
 }
